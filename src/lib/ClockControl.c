32d98268221531c0c2f39cd6310f8323f65908e2
Ilya Vasilyev <vasilya93@gmail.com>
2014-11-06 20:42:22 +0400

    Ported USART, ClockControl and Timer function to the microcontroller.

---
+++ src/lib/ClockControl.c
@@ -0,0 +1,42 @@
+#include "stm32f10x.h"
+#include "ClockControl.h"
+
+ClockControlType ClockControl;
+
+void ClockControl_Initialize()
+{  
+  //enable MSI
+  RCC->CR |= RCC_CR_HSION;
+  while(~RCC->CR & RCC_CR_HSIRDY);
+  
+  //run MSI as SYSCLK
+  RCC->CFGR &= ~(RCC_CFGR_SW);
+  while(RCC->CFGR & RCC_CFGR_SWS);
+  
+  //disable PLL to configure it
+  RCC->CR &= ~RCC_CR_PLLON;
+  while(RCC->CR & RCC_CR_PLLRDY);
+
+  RCC->CFGR &= ~RCC_CFGR_PLLSRC; //HSI
+  RCC->CFGR &= ~RCC_CFGR_PLLMULL;
+  RCC->CFGR |= RCC_CFGR_PLLMULL_1; //multiply by 4
+  RCC->CFGR &= ~RCC_CFGR_HPRE; //no division
+  RCC->CFGR &= ~RCC_CFGR_PPRE1;
+  RCC->CFGR |= RCC_CFGR_PPRE1_0 | RCC_CFGR_PPRE1_2; //devide by 4
+  RCC->CFGR &= ~RCC_CFGR_PPRE2;
+  RCC->CFGR |= RCC_CFGR_PPRE2_0 | RCC_CFGR_PPRE2_2; //devide by 4
+
+  //enable PLL
+  RCC->CR |= RCC_CR_PLLON;
+  while(~RCC->CR & RCC_CR_PLLRDY);
+  
+  //run PLL as SYSCLK
+  RCC->CFGR &= ~(RCC_CFGR_SW);
+  RCC->CFGR |= RCC_CFGR_SW_PLL;
+  while(~RCC->CFGR & RCC_CFGR_SWS_PLL);
+
+  ClockControl.APB1_Frequency = 8000000; // if changes, clock tuning in I2C should be changed 
+  ClockControl.APB2_Frequency = 8000000;
+  ClockControl.AHB_Frequency = 32000000;
+  ClockControl.SYSCLK_Frequency = 32000000;
+}
--- src/main.c
+++ src/main.c
@@ -1,27 +1,33 @@
 #include "stm32f10x.h"
+#include "ClockControl.h"
+#include "Timer.h"
+#include "USART.h"
 
 int led_state = 0;
 
 void init_all(void);
 void delay(unsigned int cycles_num);
 void toggle_led(void);
+void check_USART(void);
 
 int main(void)
 {
-	init_all();
+  init_all();
 
-	while(1) {
-		delay(5000000);
-		toggle_led();
-	}
+  while(1);
 }
 
 void init_all(void)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	GPIOA->CRL |= GPIO_CRL_MODE0_1 | GPIO_CRL_MODE1_1 | GPIO_CRL_MODE2_1 | GPIO_CRL_MODE3_1;
-	GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_CNF1 | GPIO_CRL_CNF2 | GPIO_CRL_CNF3);
-	GPIOD->ODR &= ~(GPIO_ODR_ODR0 | GPIO_ODR_ODR1 | GPIO_ODR_ODR2 | GPIO_ODR_ODR3);
+  ClockControl_Initialize();
+  USART_Begin(115200);
+  Timer_init(TIMER6);
+  Timer_start(TIMER6, &check_USART, 1000000, true);
+  
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+  GPIOA->CRL |= GPIO_CRL_MODE0_1 | GPIO_CRL_MODE1_1 | GPIO_CRL_MODE2_1 | GPIO_CRL_MODE3_1;
+  GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_CNF1 | GPIO_CRL_CNF2 | GPIO_CRL_CNF3);
+  GPIOD->ODR &= ~(GPIO_ODR_ODR0 | GPIO_ODR_ODR1 | GPIO_ODR_ODR2 | GPIO_ODR_ODR3);
 }
 
 void delay(unsigned int cycles_num)
@@ -38,4 +44,9 @@ void toggle_led(void)
 		led_state = 1;
 		GPIOA->ODR |= GPIO_ODR_ODR0 | GPIO_ODR_ODR1 | GPIO_ODR_ODR2 | GPIO_ODR_ODR3;
 	}
+}
+
+void check_USART(void)
+{
+  USART_WriteByte('h');
 }
